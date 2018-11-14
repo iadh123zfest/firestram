81da9a6baf06e7953c217b431069b0651176e830
yoyothatsmeni <44788338+yoyothatsmeni@users.noreply.github.com>
2018-11-14 22:59:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,27 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require('discord.js');
 
-client.on('ready', () => {
-  console.log('fire');
- 
-});
- 
-
-var PrEfix = "-";
+const Util = require('discord.js');
+    client = new Discord.Client({sisableEveryone: true})
+ const devs = ['408374252291751976'];
+const adminprefix = "!";
 client.on('message', message => {
-  if (!message.content.startsWith(PrEfix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '408374252291751976') return;
-if (message.content.startsWith(PrEfix + 'wt')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**✅  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith(PrEfix + 'ls')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**✅  : ${argresult}**`)
-} else 
-if (message.content.startsWith(PrEfix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/amira");
-    message.channel.sendMessage(`**✅  : ${argresult}**`)
-}
-
-});
-
-client.login(process.env.BOT_KEY);
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/Taaino");
+      message.channel.sendMessage(`**  ${argresult} تم تغيير الحاله الي :white_check_mark:**`)
+  }
+  });
